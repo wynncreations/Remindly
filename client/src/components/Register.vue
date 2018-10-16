0ad16c79711f90c92b069ce8ce0f2078a8002dbd
robert wynn <wynncreations@gmail.com>
2018-10-16 11:43:39 -0500

    adding watches and 2 way data binding to email

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h1>Register</h1>
-    <input type="email" name="email" placeholder="email"/>
-    <input type="password" name="password" placeholder="password"/>
+    <input type="email" v-model="email" name="email" placeholder="email"/>
+    <input type="password" v-model="password" name="password" placeholder="password"/>
     <br>
     <button>Register</button>
   </div>
@@ -13,6 +13,16 @@ export default {
   name: 'Register',
   data () {
     return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has change', value)
+    },
+    password (value) {
+      console.log('password has changed', value)
     }
   }
 }
