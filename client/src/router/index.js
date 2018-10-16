cdc841d86e803da3bdc43257c73d8c33986aac70
robert wynn <wynncreations@gmail.com>
2018-10-16 10:33:07 -0500

    removed search, lets do it later.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Register from '@/components/Register'
-import Search from '@/components/Search'
+
 Vue.use(Router)
 
 export default new Router({
@@ -16,11 +16,6 @@ export default new Router({
       path: '/register',
       name: 'register',
       component: Register
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: Search
     }
   ]
 })
