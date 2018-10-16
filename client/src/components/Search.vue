d3545e1088aa57788a7c4ce4bdba06e704e3f2e7
Robert <wynncreations@gmail.com>
2018-10-15 23:38:09 -0500

    added search component.

---
+++ client/src/components/Search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Zip Search</h1>
+    <input type="text" name="zip" placeholder="Zip"/>
+    <br>
+    <button>Search Zip</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
