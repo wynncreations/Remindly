9289d6b9cc5a2588583ea3b059fdff01c5d4821d
robert wynn <wynncreations@gmail.com>
2018-10-19 08:50:50 -0500

    parsing weather data

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -3,8 +3,9 @@
     <h1>Search</h1>
     <input type="text" v-model="zip" name="zip" placeholder="Zipcode"/>
     <br>
-    <button>Search</button>
+    <button @click="getData">Search</button>
     {{weather}}
+    {{zip}}
   </div>
 </template>
 
@@ -14,13 +15,22 @@ export default {
   name: 'Search',
   data () {
     return {
-      weather: ''
+      weather: [{}],
+      zip: ''
     }
   },
-  mounted () {
-    axios
-      .get('http://api.openweathermap.org/data/2.5/forecast?zip=75110,us&APPID=def77ba41b64b1a678fa39e248d9225a')
-      .then(response => (this.weather = response.data.list))
+  methods: {
+    getData () {
+      axios
+        .get('http://api.openweathermap.org/data/2.5/forecast?zip=75110,us&APPID=def77ba41b64b1a678fa39e248d9225a')
+        .then((response) => {
+          for (var i = 0; i < response.data.list.length; i++) {
+            this.weather.push(JSON.stringify(response.data.list[i]))
+            console.log(response.data.list[i])
+          }
+        })
+      return this.weather[0]
+    }
   }
 }
 </script>
