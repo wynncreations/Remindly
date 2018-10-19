632c5a546b57d7128c8c69a96d518de350e1a2d5
robert wynn <wynncreations@gmail.com>
2018-10-19 10:17:03 -0500

    added vue-notifications

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <h1>Search</h1>
-    <input type="text" v-model="zip" name="zip" placeholder="Zipcode"/>
-    <br>
-    <button @click="getData">Search</button>
-    {{weather}}
-    {{zip}}
-    {{error}}
+    <div class="container">
+      <notifications group="error" />
+      <h1>Search</h1>
+      <input type="text" v-model="zip" name="zip" placeholder="Zipcode"/>
+      <br>
+      <button @click="getData">Search</button>
+      {{weather}}
+      {{zip}}
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Notifications from 'vue-notification'
 export default {
   name: 'Search',
   data () {
@@ -27,20 +30,20 @@ export default {
         .get('http://api.openweathermap.org/data/2.5/forecast?zip=' + this.zip + ',us&APPID=def77ba41b64b1a678fa39e248d9225a')
         .catch((error) => {
           this.error = error.response.data.message
+          notify ({
+            group: 'error',
+            title: 'Error!',
+            text: error.response.data.message
+          })
         })
         .then((response) => {
-          for (var i = 39; i < response.data.list.length; i++) {
-            this.weather = response.data.list[i]
-            console.log(response.data.list[i])
-          }
+          this.weather = response.data.list
         })
-      this.weather = JSON.parse(this.weather)
-      return this.weather.clouds
     }
   }
 }
 </script>
 
 <style scoped>
-
+  @import 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css'
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Notifications from 'vue-notification'
 
 Vue.config.productionTip = false
-
+Vue.use(Notifications)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
