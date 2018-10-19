f182eb9bc5f0952b9bbee15a305bbcec59b6013e
robert wynn <wynncreations@gmail.com>
2018-10-19 10:30:39 -0500

    Made notifications more appropriate.

--- client/src/components/Search.vue
+++ client/src/components/Search.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
     <div class="container">
-      <notifications group="error" />
+      <notifications group="error" position="center top" width="30%"/>
       <h1>Search</h1>
       <input type="text" v-model="zip" name="zip" placeholder="Zipcode"/>
       <br>
       <button @click="getData">Search</button>
       {{weather}}
-      {{zip}}
     </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import Notifications from 'vue-notification'
 export default {
   name: 'Search',
   data () {
@@ -30,11 +28,13 @@ export default {
         .get('http://api.openweathermap.org/data/2.5/forecast?zip=' + this.zip + ',us&APPID=def77ba41b64b1a678fa39e248d9225a')
         .catch((error) => {
           this.error = error.response.data.message
-          notify ({
+          this.$notify({
             group: 'error',
+            type: 'warn',
             title: 'Error!',
-            text: error.response.data.message
+            text: error.response.data.message + ' Please try a valid Zip Code!'
           })
+          this.weather = ''
         })
         .then((response) => {
           this.weather = response.data.list
